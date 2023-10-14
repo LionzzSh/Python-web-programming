e631b4bb9f245d87b7a924ab92e4dd8ab5c8fbd0
LionzzSh <tvfeka@gmail.com>
2023-10-14 19:09:39 +0100

    Set up messages and data processing on the server

--- Lab 1/Ex1/1/server.py
+++ Lab 1/Ex1/1/server.py
@@ -13,3 +13,24 @@ server_socket.bind(server_address)
 
 # Режим очікування підключень, може обслуговувати до 5 клієнтів одночасно
 server_socket.listen(5)
+
+# Виводимо повідомлення про те, що сервер працює та вказуємо адресу та порт
+print("Сервер працює на: {}:{}".format(*server_address))
+
+while True:
+    # Приймаємо запит на підключення від клієнта
+    client_socket, client_address = server_socket.accept()
+    
+    # Виводимо повідомлення про підключеного користувача та його адресу
+    print("Під'єднався користувач: {}".format(client_address))
+
+    # Очікуємо та приймаємо дані від клієнта (максимум 1024 байти)
+    data = client_socket.recv(1024)
+
+    # Якщо дані отримано, виводимо їх та час отримання
+    if data:
+        print("Отриманий текст: {}".format(data.decode('utf-8')))
+        print("Час отримання: {}".format(time.ctime()))
+
+    # Закриваємо сокет для даного клієнта
+    client_socket.close()
