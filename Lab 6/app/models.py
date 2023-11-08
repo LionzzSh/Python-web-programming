c0986d41e5693d4d192f84154c409755edf6b4ba
LionzzSh <tvfeka@gmail.com>
2023-11-08 12:54:13 +0000

    add description and data fix

--- Lab 6/app/models.py
+++ Lab 6/app/models.py
@@ -11,8 +11,9 @@ class Feedback(db.Model):
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    complete = db.Column(db.Boolean)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)  # Add the description field
+    complete = db.Column(db.Boolean, default=False)
 
     def __init__(self, title, description=None, complete=False):
         self.title = title
