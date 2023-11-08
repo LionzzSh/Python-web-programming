80c108477d0ed69e5f37652472546ee44e08d7cf
LionzzSh <tvfeka@gmail.com>
2023-11-08 20:07:02 +0000

    fixed

--- Lab 6/app/forms.py
+++ Lab 6/app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, SubmitField, BooleanField, PasswordField  # Імпортуємо PasswordField
+from wtforms.validators import DataRequired, Length
 
 class FeedbackForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
@@ -12,3 +12,9 @@ class TodoForm(FlaskForm):
     description = StringField('Description')
     complete = BooleanField('Complete')
     submit = SubmitField('Add Todo')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=4, max=10)])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Log In')
--- Lab 6/app/views.py
+++ Lab 6/app/views.py
@@ -1,3 +1,5 @@
+
+
 import json
 import os
 import io
@@ -8,33 +10,40 @@ from flask import render_template, request, session, redirect, url_for, flash, m
 from app import app, db
 from app.forms import FeedbackForm
 from app.models import Feedback
+from app import app, db
+from app.forms import LoginForm  
 from app.models import Todo
 from app.forms import TodoForm
 
 app.secret_key = b'secret'
 
-users_json_path = 'Lab 6/app/static/files/users.json' 
+# Path to the JSON file containing user data
+users_json_path = 'Lab 5/app/static/files/users.json'
 
+# Load user data from the JSON file
 with open(users_json_path, 'r') as users_file:
     users_data = json.load(users_file)
 
+# Function to save user data to the JSON file
 def save_users_data():
     with open(users_json_path, 'w') as users_file:
         json.dump(users_data, users_file, indent=4)
 
+# Common data for templates
 common = {
     'first_name': 'Vitalii',
     'last_name': 'Shmatolokha',
 }
 
+# Function to get the absolute path of a static file
 def get_static_file(path):
     site_root = os.path.realpath(os.path.dirname(__file__))
     return os.path.join(site_root, path)
 
+# Function to get JSON data from a static file
 def get_static_json(path):
     with open(get_static_file(path), "r", encoding="utf-8") as file:
         return json.load(file)
-
 @app.route('/todo')
 def home():
     todo_list = db.session.query(Todo).all()
@@ -65,21 +74,24 @@ def delete(todo_id):
     db.session.delete(todo)
     db.session.commit()
     return redirect(url_for("home"))
-
+# Route for the home page
 @app.route('/')
 def index():
     return render_template('home.html', common=common)
 
+# Route for the biography page
 @app.route('/biography')
 def biography():
     biography = get_static_json("static/files/biography.json")
     return render_template('biography.html', common=common, biography=biography)
 
+# Route for the skills page
 @app.route('/skills')
 def skills():
     data = get_static_json("static/files/skills.json")
     return render_template('skills.html', common=common, data=data)
 
+# Route for the projects page
 @app.route('/projects')
 def projects():
     data = get_static_json("static/projects/projects.json")['projects']
@@ -92,12 +104,14 @@ def projects():
 
     return render_template('projects.html', common=common, projects=data, tag=tag)
 
+# Route for the experiences page
 @app.route('/experiences')
 def experiences():
     experiences = get_static_json("static/experiences/experiences.json")['experiences']
     experiences.sort(key=lambda x: x.get('weight', 0), reverse=True)
     return render_template('projects.html', common=common, projects=experiences, tag=None)
 
+# Route for individual project or experience
 @app.route('/projects/<title>')
 def project(title):
     projects = get_static_json("static/projects/projects.json")['projects']
@@ -143,10 +157,12 @@ def feedback():
     feedback_data = Feedback.query.all()
     return render_template('feedback.html', form=form, feedback_data=feedback_data, common=common)
 
+# Error handler for 404 Not Found
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html', common=common), 404
 
+# Context processor to provide additional data to templates
 @app.context_processor
 def utility_processor():
     os_info = platform.platform()
@@ -158,29 +174,34 @@ def utility_processor():
         'current_time': current_time
     }
 
+# Route for the login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
 
         if username in users_data:
             if users_data[username]['password'] == password:
                 user_info = users_data[username]
                 session['user_info'] = user_info
-                flash('Успішний вхід', 'success')
+                flash('Ваші дані було збережено', 'success')  # Flash message for successful login
                 return redirect(url_for('info'))
             else:
-                flash('Невірний пароль', 'error')
+                flash('Невірний пароль', 'error')  # Flash message for incorrect password
         else:
-            flash('Невірне імя користувача', 'error')
+            flash('Невірне імя користувача', 'error')  # Flash message for incorrect username
 
     if 'user_info' in session:
-        flash('Ви вже увійшли', 'info')
+        flash('Ви вже увійшли', 'info')  # Flash message for already logged in
         return redirect(url_for('info'))
 
-    return render_template('login.html', common=common)
 
+    return render_template('login.html', form=form, common=common)
+
+# Route for the user info page
 @app.route('/info', methods=['GET', 'POST'])
 def info():
     user_info = session.get('user_info')
@@ -238,7 +259,6 @@ def info():
     else:
         flash('Ви повинні увійти, щоб отримати доступ до цієї сторінки', 'error')
         return redirect(url_for('login'))
-    
 
 if __name__ == '__main__':
     app.run(debug=True)
