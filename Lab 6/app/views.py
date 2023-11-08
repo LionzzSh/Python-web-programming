0d512532e0497e7298a46278ac452d698d831bda
LionzzSh <tvfeka@gmail.com>
2023-11-08 20:36:30 +0000

    fix

--- Lab 6/app/views.py
+++ Lab 6/app/views.py
@@ -18,7 +18,7 @@ from app.forms import TodoForm
 app.secret_key = b'secret'
 
 # Path to the JSON file containing user data
-users_json_path = 'Lab 5/app/static/files/users.json'
+users_json_path = 'Lab 6/app/static/files/users.json'
 
 # Load user data from the JSON file
 with open(users_json_path, 'r') as users_file:
